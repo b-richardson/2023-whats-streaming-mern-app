ff663b52e696eb5b1ba253f0dc59c50bdef7c544
odelavia <odelavia@alpine-consulting.com>
2023-04-06 17:19:38 -0600

    remove console.log

--- client/src/components/common/BackdropSlide.jsx
+++ client/src/components/common/BackdropSlide.jsx
@@ -3,22 +3,19 @@ import { SwiperSlide } from "swiper/react";
 import tmdbConfigs from "../../api/configs/tmdb.configs";
 import NavigationSwiper from "./NavigationSwiper";
 
-const BackdropSlide = ({ backdrops }) => {
-  console.log("entered the backdropslide: ", backdrops)
-  return (
-    <NavigationSwiper>
-      {[...backdrops].splice(0, 10).map((backdrop, index) => (
-        <SwiperSlide key={index}>
-          <Box sx={{
-            paddingTop: "60%",
-            backgroundPosition: "top",
-            backgroundSize: "cover",
-            backgroundImage: `url(${tmdbConfigs.backdropPath(backdrop.file_path)})`
-          }} />
-        </SwiperSlide>
-      ))}
-    </NavigationSwiper>
-  );
-};
+const BackdropSlide = ({ backdrops }) => (
+  <NavigationSwiper>
+    {[...backdrops].splice(0, 10).map((backdrop, index) => (
+      <SwiperSlide key={index}>
+        <Box sx={{
+          paddingTop: "60%",
+          backgroundPosition: "top",
+          backgroundSize: "cover",
+          backgroundImage: `url(${tmdbConfigs.backdropPath(backdrop.file_path)})`
+        }} />
+      </SwiperSlide>
+    ))}
+  </NavigationSwiper>
+);
 
 export default BackdropSlide;
